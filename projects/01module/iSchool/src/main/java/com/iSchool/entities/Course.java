5023136551588dddbfda6f5fdf2da701dcea0cca
mateusribeirocampos <mateusrcampos@gmail.com>
2026-02-26 19:36:46 -0300

    Chore: project ischool initialized with User and Role

---
+++ projects/01module/iSchool/src/main/java/com/iSchool/entities/Course.java
@@ -0,0 +1,9 @@
+package com.iSchool.entities;
+
+public class Course {
+
+    private Long id;
+    private String name;
+    private String imgUrl;
+    private String imgGrayUri;
+}
---
+++ projects/01module/iSchool/src/test/java/com/iSchool/ISchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
