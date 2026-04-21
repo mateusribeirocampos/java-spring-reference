8302dfa555a7a561e21460383c44c4cbf9cb1fd7
mateusribeirocampos <mateusrcampos@gmail.com>
2026-04-21 19:50:53 -0300

    Chore: ResourceExceptionHandler updated and FileDTO created to handle the endpoint to upload files

--- projects/03module/dscatalog-s3-v345/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ projects/03module/dscatalog-s3-v345/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -2,28 +2,16 @@ package com.devsuperior.dscatalog;
 
 import com.devsuperior.dscatalog.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 @SpringBootApplication
-public class DscatalogApplication implements CommandLineRunner {
+public class DscatalogApplication {
 
 	@Autowired
 	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		String path = "/Users/mateusribeirodecampos/Desktop/profile.png";
-		try (InputStream is = new FileInputStream(path)) {
-			s3Service.uploadFile(is, "profile.png", "image/png");
-		}
 	}
 }
